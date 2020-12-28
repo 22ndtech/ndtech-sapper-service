703cd0183cd59627bd0f4c99c985aa88eb113f7a
Jeff Hegedus <jeff@22ndtech.com>
2020-12-28 08:33:36 -0700

    added github login support

---
+++ src/routes/logged-in/index.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { onMount } from 'svelte';
+  // import qs from 'query-string';
+
+  let props = {};
+  let userName = "";
+
+  onMount(async()=>{
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    userName = urlSearchParams.get("userName");
+
+    // const query = qs.parse(context.queryString);
+    
+  })
+</script>
+
+<h1>Welcome to 22ndtech {userName}</h1>
